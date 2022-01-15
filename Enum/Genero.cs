4e00f9448cc3a43721b62bee6a1fa782a312fe3a
Mayara <mayaracxmarques@gmail.com>
2022-01-14 22:19:44 -0300

    Projeto DIO

---
+++ Enum/Genero.cs
@@ -0,0 +1,15 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Drama = 4,
+		Espionagem = 5,
+		Fantasia = 6,
+		Musical = 7,
+		Romance = 8,
+		Suspense = 9
+	}
+}
